24f9d6a82460af5aaa3de219d02b003e4926e05f
victorcwyu <victorcwyu@gmail.com>
2020-02-17 22:13:39 +0000

    Added a header partial

--- express_server.js
+++ express_server.js
@@ -30,8 +30,6 @@ app.get("/urls", (req, res) => {
   res.render("urls_index", templateVars);
 });
 
-
-
 app.get("/urls/:shortURL", (req, res) => {
   let templateVars = { shortURL: req.params.shortURL, longURL: urlDatabase[req.params.shortURL] };
   res.render("urls_show", templateVars);
