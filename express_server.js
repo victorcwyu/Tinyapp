fb87bad552d0e034649c614aea6e9e6ee7f1a4f4
victorcwyu <victorcwyu@gmail.com>
2020-02-17 22:49:21 +0000

    Added a POST route to receive the form submission

--- express_server.js
+++ express_server.js
@@ -41,3 +41,8 @@ app.get("/urls/:shortURL", (req, res) => {
   let templateVars = { shortURL: req.params.shortURL, longURL: urlDatabase[req.params.shortURL] };
   res.render("urls_show", templateVars);
 });
+
+app.post("/urls", (req, res) => {
+  console.log(req.body);  // Log the POST request body to the console
+  res.send("Ok");         // Respond with 'Ok' (we will replace this)
+});
