d1cdd657a59c99343e1eb85dca71f3fd592d0b05
victorcwyu <victorcwyu@gmail.com>
2020-02-17 17:50:46 +0000

    Add endpoint/route

--- express_server.js
+++ express_server.js
@@ -14,3 +14,7 @@ app.get("/", (req, res) => {
 app.listen(PORT, () => {
   console.log(`Example app listening on port ${PORT}!`);
 });
+
+app.get("/urls.json", (req, res) => {
+  res.json(urlDatabase);
+});
